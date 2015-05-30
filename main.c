e6e44fb18f7a66ad51fbbb8d4b6fd29c5b0855c1
Frederico Barnard <fredericobf@gmail.com>
2015-05-30 03:27:08 -0300

    Test file

--- main.c
+++ main.c
@@ -3,9 +3,27 @@
 
 int main (int argc, char ** argv)
 {
-  st_head * test = create_structure();
+    char str1[1] = "{";
+    char str2[2] = "{{";
 
-  printf("%d\n", test->levels);
+    char ** ret_array;
+    st_head * json_runner;
 
-  return 0;
+    json_runner = loadJson(str1);
+    ret_array = getFieldValue(json_runner, "nome1");
+    printf("%s - %s\n", ret_array[0], ret_array[1]);
+
+    json_runner = loadJson(str2);
+    ret_array = getFieldValue(json_runner, "nome2");
+    printf("%s - %s\n", ret_array[0], ret_array[1]);
+
+    /*json_runner = create_structure();
+
+    setFieldValues(json_runner, "nome", "string", "Fred");
+
+    ret_array = getFieldValue(json_runner, "nome");
+
+    printf("%s - %s\n", ret_array[0], ret_array[1]);*/
+
+    return 0;
 }
